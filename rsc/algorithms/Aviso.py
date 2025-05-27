b45c5d236d8eea585988167f4a880f406e6f9d48
Pshenri <henriquepedro4041@gmail.com>
2025-05-27 16:14:21 -0300

    Update Aviso.py

--- rsc/algorithms/Aviso.py
+++ rsc/algorithms/Aviso.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # df = pd.read_csv('resultado_anomalias.csv', 'resultado_anomalias_Placas.csv', 'resultados_anomalias_Facial.csv')
 # anomalias_detectadas = df[df['anomalia'] == 1]
